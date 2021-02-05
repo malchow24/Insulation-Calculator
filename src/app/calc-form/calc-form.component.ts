8ee4e89b429a13701106635d8dcd40fa7a83464b
Owen <omalchow@uwm.edu>
2021-02-05 17:24:44 -0600

    adding service/working on form controls

--- src/app/calc-form/calc-form.component.ts
+++ src/app/calc-form/calc-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { tap } from 'rxjs/operators';
 import { CeilingInsulation } from './calc-services';
 
@@ -9,24 +9,37 @@ import { CeilingInsulation } from './calc-services';
   styleUrls: ['./calc-form.component.css']
 })
 export class CalcFormComponent implements OnInit {
-  formControl = new FormControl()
-  wallInsulation = ['R13', 'R13 + R6.5 C.I.', 'R15', 'R15 + R6.5 C.I.', 'R15 + R13 C.I.'];
-  buildingDetails = ['Length (In.)', 'Width (In.)', 'Height (In.)'];
-  openings = ['3070 Door', '3080 Door', '4080 Door', '6080 Door', '2 Ton HVAC', '2.5 Ton HVAC', '3 Ton HVAC', '3.5 Ton HVAC', '4 Ton HVAC', '5 Ton HVAC', '6 Ton HVAC', 'EQ Door Length']
+  ceilingInsulation: {ceilingType: string}[] = [];
 
-  constructor(private ceilingInsulation: CeilingInsulation) { }
+  public formGroup:FormGroup = new FormGroup({
+    ceiling: new FormControl(''),
+    walls: new FormControl(''),
+    sheetrock: new FormControl('')
+  });
+
+  public formGroup2:FormGroup = new FormGroup({
+    length: new FormControl(''),
+    width: new FormControl(''),
+    height: new FormControl(''),
+  })
+
+
+  wallInsulation = ['R13', 'R13 + R6.5 C.I.', 'R15', 'R15 + R6.5 C.I.', 'R15 + R13 C.I.'];
+
+
+  constructor(private ceilingService: CeilingInsulation) { }
 
   ngOnInit(): void {
-    this.formControl.valueChanges.pipe(
+    this.ceilingInsulation = this.ceilingService.ceilingInsulation;
+    console.log(this.ceilingInsulation)
+    
+    /* this.formControl.valueChanges.pipe(
       tap((val) => {
         console.log(val);
         
       })
-    ).subscribe();
+    ).subscribe(); */
   }
-
-  
-
 }
 //Three seperate form groups
 //ng build --prod
--- src/app/calc-form/calc-services.ts
+++ src/app/calc-form/calc-services.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 
-@Injectable()
+@Injectable({
+    providedIn: 'root',
+})
 export class CeilingInsulation {
     ceilingInsulation = [
         {
@@ -16,17 +18,4 @@ export class CeilingInsulation {
             ceilingType: 'R49'
         }
     ];
-
-    getCeiling = (name: string) => {
-        if(name === "R15") {
-            console.log('R15');
-        } else if (name === "R30") {
-            console.log("R30"); 
-        } else if (name === "R38") {
-            console.log("R38"); 
-        } else {
-            console.log("R49");
-            
-        }
-    }
 }   
