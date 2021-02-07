266c5a60a9e709a1c75512313042f839edbb3720
Owen <omalchow@uwm.edu>
2021-02-06 19:03:59 -0600

    working on calculations etc

--- src/app/calc-form/calc-form.component.ts
+++ src/app/calc-form/calc-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Form, FormControl, FormGroup } from '@angular/forms';
 import { tap } from 'rxjs/operators';
-import { InsulationService } from './calc-services';
+import { InsulationService } from '../calc-services';
 
 @Component({
   selector: 'app-calc-form',
@@ -12,7 +12,17 @@ export class CalcFormComponent implements OnInit {
   //Variables
   ceilingInsulation: string[];
   wallInsulation: string[];
+  /* openings: string[]; */
   sheetrock: string[];
+  length: number;
+  width: number;
+  height: number;
+  ceilingOutput: string;
+  wallOutput: string;
+  firecodeOutput: string; 
+  wallBatts: number;
+  openings: number;
+
 
   //Insulation FormGroup
   public insulationDetails:FormGroup = new FormGroup({
@@ -26,8 +36,24 @@ export class CalcFormComponent implements OnInit {
   public buildingDetails:FormGroup = new FormGroup({
     length: new FormControl(''),
     width: new FormControl(''),
-    height: new FormControl(''),
-  })
+    height: new FormControl('')
+  });
+
+  //Openings FormGroup
+  public Openings:FormGroup = new FormGroup({
+    door3070: new FormControl(''),
+    door3080: new FormControl(''),
+    door4080: new FormControl(''),
+    door6080: new FormControl(''),
+    hvac2: new FormControl(''),
+    hvac2Half: new FormControl(''),
+    havc3: new FormControl(''),
+    hvac3Half: new FormControl(''),
+    hvac4: new FormControl(''),
+    hvac5: new FormControl(''),
+    hvac6: new FormControl(''),
+    eqDoors: new FormControl('')
+  });
 
   //Getting services
   constructor(private insulationService: InsulationService) { }
@@ -36,12 +62,37 @@ export class CalcFormComponent implements OnInit {
     this.ceilingInsulation = this.insulationService.ceilingInsulation;
     this.wallInsulation = this.insulationService.wallInsulation;
     this.sheetrock = this.insulationService.sheetrock;
+    this.length = this.buildingDetails.controls.length.value;
+    this.width = this.buildingDetails.controls.width.value;
+    this.height = this.buildingDetails.controls.height.value;
+    this.openings = 37126;
 
-    
+    this.buildingDetails.controls.length.valueChanges.pipe(
+      tap((val) => {
+        this.length = val;
+        return this.length;
+      })
+    ).subscribe();
+
+    this.buildingDetails.controls.width.valueChanges.pipe(
+      tap((val) => {
+        this.width = val;
+        return this.width;
+      })
+    ).subscribe();
+
+    this.buildingDetails.controls.height.valueChanges.pipe(
+      tap((val) => {
+        this.height = val;
+        return this.height;
+      })
+    ).subscribe();
+
+
     //Set the value of the Ceiling
     this.insulationDetails.controls.ceiling.valueChanges.pipe(
       tap((val) => {
-        this.insulationService.setCeilingType(val);
+        this.ceilingOutput = this.insulationService.setCeilingType(val, this.length, this.width); 
       })
     ).subscribe();
 
@@ -49,14 +100,14 @@ export class CalcFormComponent implements OnInit {
     //Set the value of the walls
     this.insulationDetails.controls.walls.valueChanges.pipe(
       tap((val) => {
-        this.insulationService.setWallType(val);
+        this.wallOutput = this.insulationService.setWallType(val, this.length, this.width, this.height, this.openings);
       })
     ).subscribe();
 
     //Set the value of sheetroc to y/n
     this.insulationDetails.controls.sheetrock.valueChanges.pipe(
       tap((val) => {
-        this.insulationService.setSheetrock(val);
+        this.firecodeOutput = this.insulationService.setSheetrock(val);
       })
     ).subscribe();
   };
---
+++ src/app/calc-services.ts
@@ -0,0 +1,78 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class InsulationService {
+    ceilingInsulation = ['R15', 'R30', 'R38', 'R49'];
+    sheetrock = ['Yes', 'No'];
+    wallInsulation = ['R13', 'R13 + R6.5 C.I', 'R15', 'R15 + R6.5 C.I', 'R15 + R13 C.I'];
+    openings = ['3070 Door', '3080 Door', '4080 Door', '6080 Door', '2 Ton HVAC', '2.5 Ton HVAC', '3 Ton HVAC', '3.5 Ton HVAC', 
+'3.5 Ton HVAC', '4 Ton HVAC', '5 Ton HVAC', '6 Ton HVAC', 'EQ Door Length'];
+    length: number; 
+    width: number;
+    height: number;
+    batts: number;
+    batts2: number;
+    wallbatts: number;
+    wallsheets: number;
+
+    onChange(UpdatedValue: number) {
+        this.length = UpdatedValue;
+    }
+    onWidthChange(UpdatedValue: number) {
+        this.width = UpdatedValue;
+    }
+    onHeightChange(UpdatedValue: number) {
+        this.height = UpdatedValue;
+    }
+
+
+    setCeilingType(val: String, length: number, width: number) {
+        if(val === 'R15') {
+            this.batts = (length * width / 144 / 77.5 * 0.9375);
+            return `Ceiling -  R15 Batts: ${this.batts}`;
+        } else if(val === 'R30') {
+            this.batts = (length * width * 1.5 / 144 / 77.5 * 0.9375);
+            return `Ceiling - R15 Batts: ${this.batts}`;
+        } else if (val === 'R38') {
+            this.batts = (length * width / 144 / 67.81 * 0.9375 * 1.5);
+            return `Ceiling - R21 Batts: ${this.batts}`;
+        } else if (val === 'R49') {
+            this.batts = (length * width * 1.5 / 144 / 77.5 * 0.9375);
+            this.batts2 = (length * width / 144 / 67.81 * 0.9375 * 0.5);
+            return `Ceiling - R21 Batts: ${this.batts2} | R15 Batts: ${this.batts}`;
+        }
+    };
+
+    setWallType(val: string, length: number, width: number, height: number, openings: number) {
+        if(val === 'R13') {
+            this.wallbatts = ((length * (height - 21) * 2 - openings)/144 + (width * (height - 21) * 2)/144) / 116.3 * 0.9375;
+            return `Wall - R13 Batts: ${this.wallbatts}`;
+        } else if(val === 'R13 + R6.5 C.I') {
+            this.wallbatts = ((length * (height - 21) * 2 - openings)/144 + (width * (height - 21) * 2)/144) / 116.3 * 0.9375;
+            this.wallsheets = ((length * (height - 21) * 2 - openings) / 144 + (width * (height - 21) * 2) / 144) / 32 * 1.2;
+            return `Wall - R13 Batts: ${this.wallbatts} | 1" Thermax Sheets: ${this.wallsheets}`;
+        } else if (val === 'R15') {
+            this.wallbatts = ((length * (height - 21) * 2 - openings) / 144 + (width * (height - 21) * 2) / 144) / 77.5 * 0.9375;
+            return `Wall - R15 Batts: ${this.wallbatts}`;
+        } else if (val === 'R15 + R6.5 C.I') {
+            this.wallbatts = ((length * (height - 21) * 2 - openings) / 144 + (width * (height - 21) * 2) / 144) / 77.5 * 0.9375;
+            this.wallsheets = ((length * (height - 21) * 2 - openings) / 144 + (width * (height - 21) * 2) / 144) / 32 * 1.2;
+            return `Wall - R15 Batts: ${this.wallbatts} | 1" Thermax Sheets: ${this.wallsheets}`;
+        } else if (val === 'R15 + R13 C.I') {
+            this.wallbatts = ((length * (height - 21) * 2 - openings) / 144 + (width * (height - 21) * 2) / 144) / 77.5 * 0.9375;
+            this.wallsheets = ((length * (height - 21) * 2 - openings) / 144 + (width * (height - 21) * 2) / 144) / 32 * 1.2 * 2;
+            return `Wall - R15 Batts: ${this.wallbatts} | 1" Thermax Sheets: ${this.wallsheets}`;
+        } 
+    };
+
+    setSheetrock(val: string) {
+        if(val === 'Yes') {
+            return `Sheets Required: `;
+        } else if(val === 'No') {
+            console.log('None');
+        }
+    };    
+}   
+
